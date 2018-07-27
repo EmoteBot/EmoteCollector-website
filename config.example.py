0b937bb0d08969ede39b136b785d64995a0b7d2e
bmintz <bmintz@protonmail.com>
2018-07-27 02:11:53 -0500

    move it back to website/
    this is for git filter-branch --subdirectory-filter

---
+++ config.example.py
@@ -0,0 +1,15 @@
+{
+	'onions': {
+		# tor onion services for your instance
+		# if left blank, the links to these will not be shown from the clearnet site.
+		2: '', # example: 'http://emotesqil3yd4zb3.onion/list'
+		3: '', # example: 'http://emotesdikhisgxdcmh7wtlvzfw2yxp4vmkyy6mu5wixzgqfmxvuotryd.onion/list'
+	},
+
+	'database': {
+		'user': 'connoisseur',
+		'password': 'hunter2',
+		'database': 'connoisseur',
+		'host': '127.0.0.1',
+		'port': 5432},
+}
