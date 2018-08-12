3a5d1d3636c07ec886c2c588ee7008cbd2b1c0e1
bmintz <bmintz@protonmail.com>
2018-08-12 06:22:24 -0500

    start.sh: launch the aiohttp API

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 gunicorn --workers 1 --threads 4 --max-requests 10000 --reload --bind 0.0.0.0:8080 app:app &
-gunicorn --workers 1 --threads 4 --max-requests 10000 --reload --bind 0.0.0.0:8081 api:app &
+gunicorn --workers 1 --worker-class aiohttp.GunicornWebWorker --threads 4 --max-requests 10000 --reload --bind 0.0.0.0:8081 api:app &
 
 wait
